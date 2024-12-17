d1fbdafd75afd7b3652213e2bc063883a715e2d8
nannipy <pernazza.1936239@studenti.uniroma1.it>
2024-12-17 11:18:33 +0100

    fix 3

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -10,22 +10,20 @@ export const projectItems = [
       "a web app for Agricola Pernazza, a farm located in Amelia, Italy. You can find information about the farm, the animals, the activities and the events and order products.",
   },
   {
-    name: "Parco La Cascina",
-    github: "https://github.com/nannipy/parcolacascina",
-    link: "https://parcolacascina.vercel.app/",
-    position: "",
-    description:
-      "a web app to search for Parco la Cascina, a farm located near Varese, Italy. You can find information about the farm, the animals, the activities and the events and order products.",
-  },
-  {
     name: "ADM - sport and nutrition",
     github: "https://github.com/nannipy/adm",
     link: "https://adm-sportandnutrition.vercel.app/",
     position: "",
     description:
       "a web app for a personal trainer, built for gym trainings and nutrition monitoring with a simple and clean UI.",
+  },
+  {
+    name: "Parco La Cascina",
+    github: "https://github.com/nannipy/parcolacascina",
+    link: "https://parcolacascina.vercel.app/",
+    position: "",
+    description:
+      "a web app to search for Parco la Cascina, a farm located near Varese, Italy. You can find information about the farm, the animals, the activities and the events and order products.",
   }
-
-
-  
+    
 ] satisfies ExperienceItem[];
